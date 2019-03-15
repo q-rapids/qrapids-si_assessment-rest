0ebec5f685bd07d3d727d14aa2cd1a0a1ce13a23
Aleix Balletbó <aleixballetbo@gmail.com>
2019-03-15 11:16:59 +0100

    Enable deployment as war

--- src/main/java/com/upc/gessi/qrapids/si_assessment_rest/SiAssessmentApplication.java
+++ src/main/java/com/upc/gessi/qrapids/si_assessment_rest/SiAssessmentApplication.java
@@ -2,9 +2,16 @@ package com.upc.gessi.qrapids.si_assessment_rest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SiAssessmentApplication {
+public class SiAssessmentApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SiAssessmentApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(SiAssessmentApplication.class, args);
